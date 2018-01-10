f0f8baaa67ce4bf92546847582bcb9bb79485944
rathr1 <RahulSingh.Rathore@in.pega.com>
2018-01-10 14:40:10 +0530

    Added Resource Helper

--- src/main/java/com/webdriver/helper/utility/ResourcePathHelper.java
+++ src/main/java/com/webdriver/helper/utility/ResourcePathHelper.java
@@ -1,8 +1,16 @@
 package com.webdriver.helper.utility;
 
+import java.io.InputStream;
+import java.net.URL;
+
+import org.apache.log4j.Logger;
+
 public class ResourcePathHelper {
 
 	private static ResourcePathHelper helper;
+	private Logger oLog = Logger.getLogger(ResourceHelper.class);
+	private String url;
+	private InputStream stream;
 	
 	public static ResourcePathHelper getInstance() {
 		if(helper == null)
@@ -11,8 +19,33 @@ public class ResourcePathHelper {
 	}
 
 	public String getBaseResourcePath() {
-		return ResourceHelper.class.getClassLoader().getResource("./").getPath();
+		String basePath = ResourceHelper.class.getClassLoader().getResource("./").getPath();
+		oLog.info(basePath);
+		return basePath;
+		
 	}
+
+	public String getResourcePath(String string) {
+		try{
+			url = ResourceHelper.class.getClassLoader().getResource(string).getPath();
+		}catch(Exception e){
+			throw new IllegalArgumentException(string);
+		}
+		return url;
+	}
+
+	public InputStream getResourcePathInputStream(String string) {
+		try {
+			stream = ResourceHelper.class.getClassLoader().getResourceAsStream(string);
+		} catch (Exception e) {
+			throw new IllegalArgumentException(string);
+		}
+		
+		if(stream == null)
+			throw new IllegalArgumentException(string);
+		return stream;
+	}
+
 
 
 }
--- src/test/java/com/webdriver/helper/utility/TestResourcePathHelper.java
+++ src/test/java/com/webdriver/helper/utility/TestResourcePathHelper.java
@@ -3,12 +3,15 @@ package com.webdriver.helper.utility;
 import static org.testng.Assert.assertNotNull;
 import static org.testng.Assert.assertTrue;
 
+import java.io.InputStream;
+
 import org.junit.Test;
 
 public class TestResourcePathHelper {
 	
 	private ResourcePathHelper helper;
 	private String path;
+	private InputStream inputStream;
 
 	@Test
 	public void test_getResourcePathInstance(){
@@ -23,6 +26,67 @@ public class TestResourcePathHelper {
 		thenAbsolutePathshouldBeReturned();
 	}
 
+	@Test(expected = IllegalArgumentException.class)
+	public void test_getResourcePathWithInvalidStringArgument(){
+		givenIhaveTheInstanceOfResourcePatHelper();
+		whenIcallTheGetResourcePathWithStringArgument("resource/config");
+		thenAbsolutePathshouldBeReturned();
+	}
+	
+	@Test
+	public void test_getResourcePath_with_valid_string_argument(){
+		givenIhaveTheInstanceOfResourcePatHelper();
+		whenIcallTheGetResourcePathWithStringArgument("config/");
+		thenAbsolutePathshouldBeReturned();
+	}
+	
+	@Test
+	public void test_getResourcePath_with_valid_empty_string_argument(){
+		givenIhaveTheInstanceOfResourcePatHelper();
+		whenIcallTheGetResourcePathWithStringArgument("");
+		thenAbsolutePathshouldBeReturned();
+	}
+	
+	@Test(expected = IllegalArgumentException.class)
+	public void test_getResourcePath_with_valid_null_string_argument(){
+		givenIhaveTheInstanceOfResourcePatHelper();
+		whenIcallTheGetResourcePathWithStringArgument(null);
+		thenAbsolutePathshouldBeReturned();
+	}
+	
+	@Test
+	public void test_getResourcePathInputStream_with_valid_string_argument(){
+		givenIhaveTheInstanceOfResourcePatHelper();
+		whenIcallTheGetResourcePathInputStreamWithArgument("config/");
+		thenInputStreamObjectshouldBeReturned();
+	}
+	
+	@Test(expected = IllegalArgumentException.class)
+	public void test_getResourcePathInputStream_with_invlaid_string_argument(){
+		givenIhaveTheInstanceOfResourcePatHelper();
+		whenIcallTheGetResourcePathInputStreamWithArgument("config/invalid");
+		thenInputStreamObjectshouldBeReturned();
+	}
+	
+	@Test(expected = IllegalArgumentException.class)
+	public void test_getResourcePathInputStream_with_null_string_argument(){
+		givenIhaveTheInstanceOfResourcePatHelper();
+		whenIcallTheGetResourcePathInputStreamWithArgument(null);
+		thenInputStreamObjectshouldBeReturned();
+	}
+	
+	private void thenInputStreamObjectshouldBeReturned() {
+		assertNotNull(inputStream,"GetResourcePathInputStream is returning the object as Null");
+	}
+
+	private void whenIcallTheGetResourcePathInputStreamWithArgument(String newParam) {
+		inputStream = helper.getResourcePathInputStream(newParam);
+	}
+
+	private void whenIcallTheGetResourcePathWithStringArgument(String newParam) {
+		path = helper.getResourcePath(newParam);
+	}
+
 	private void thenAbsolutePathshouldBeReturned() {
 		assertTrue(path.length() > 1, "Null Path is coming");
 	}
@@ -35,7 +99,6 @@ public class TestResourcePathHelper {
 		assertNotNull(helper,"ResourceHelperPath object is Null");
 		
 	}
-
 	private void givenIhaveTheInstanceOfResourcePatHelper() {
 		helper = ResourcePathHelper.getInstance();
 	}
